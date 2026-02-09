884aecf0230505e4016304c80387507ad1b12ad0
Adonis <wintiger0908@gmail.com>
2026-02-09 20:26:50 +0900

    2557 공부 및 문제 풀이

--- 입출력과 사칙연산/2557 Hello World.cpp
+++ 입출력과 사칙연산/2557 Hello World.cpp
@@ -17,3 +17,34 @@ int main()
 	scanf("%d", &a);		// 입력
 	printf("%d", a);	// 출력
 }*/
+
+/* cstdio 헤더 파일 예시
+#include <cstdio>
+
+int main() {
+	int a;
+	scanf("%d", &a);		// 입력 std::scanf도 가능
+	printf("%d", a);		// 출력 std::printf도 가능
+}*/
+
+/* stdio.h 사용 답
+#include <stdio.h>
+
+int main() {
+	printf("Hello World!");
+	return 0;
+}*/
+
+/* iostream 사용 답
+#include<iostream>
+
+int main() {
+	std::cout << "Hello World!";
+	return 0;
+}*/
+
+#include <cstdio>
+
+int main() {
+	printf("Hello World!");
+}
