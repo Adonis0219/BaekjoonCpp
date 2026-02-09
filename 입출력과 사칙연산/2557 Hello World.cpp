31b9365f0d34129c93bd88f62a33aab116ce3103
Adonis <wintiger0908@gmail.com>
2026-02-09 19:57:54 +0900

    초기 커밋.

---
+++ 입출력과 사칙연산/2557 Hello World.cpp
@@ -0,0 +1,19 @@
+﻿/* iostream 헤더 파일 예시
+#include <iostream>
+
+int main()
+{
+	int a;
+	std::cin >> a;		// 입력
+	std::cout << a;	// 출력
+}*/
+
+/* stdio.h 헤더 파일 예시
+#include<stdio.h>
+
+int main()
+{
+	int a;
+	scanf("%d", &a);		// 입력
+	printf("%d", a);	// 출력
+}*/
