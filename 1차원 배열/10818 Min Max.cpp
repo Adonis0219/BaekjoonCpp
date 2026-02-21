0a7f9036c2e4dcd664004049778e920edefb0acb
Adonis <wintiger0908@gmail.com>
2026-02-21 14:13:23 +0900

    10818 문제 풀이

--- 1차원 배열/10818 Min Max.cpp
+++ 1차원 배열/10818 Min Max.cpp
@@ -1,5 +1,7 @@
+#pragma region 배열 O
 #include<stdio.h>
 
+// int main의 범위를 넘어가서 전역으로 빼주기
 int A[1000001];
 
 int main() {
@@ -30,3 +32,48 @@ int main() {
 
 	printf("%d %d", min, max);
 }
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	int N, min = 1000000, max = -1000000, temp;
+
+	cin >> N;
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> temp;
+
+		if (temp > max) max = temp;
+		if (temp < min) min = temp;
+	}
+		cout << min << " " << max;
+}
+
+#pragma endregion
+
+#pragma region 배열 X
+
+#include<stdio.h>
+
+int main() {
+	int N, min = 1000000, max = -1000000;
+
+	scanf_s("%d", &N);
+
+	int temp;
+
+	for (int i = 0; i < N; i++)
+	{
+		scanf_s("%d", &temp);
+
+		if (temp > max) max = temp;
+		if (temp < min) min = temp;
+	}
+	printf("%d %d", min, max);
+}
+
+
+#pragma endregion
