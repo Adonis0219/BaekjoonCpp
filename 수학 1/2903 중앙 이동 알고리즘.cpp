a77d197cbea398675652797c981c2cc3e7540d58
Adonis <wintiger0908@gmail.com>
2026-03-27 23:19:41 +0900

    2903 문제 풀이

---
+++ 수학 1/2903 중앙 이동 알고리즘.cpp
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+int main()
+{
+	int dot = 2;
+	int n;
+
+	scanf_s("%d", &n);
+
+	while (n--)
+	{
+		dot = dot * 2 - 1;
+	}
+
+	printf("%d", dot * dot);
+
+	return 0;
+}
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	int dot = 2;
+	int n;
+
+	cin >> n;
+
+	while (n--)
+	{
+		dot = dot * 2 - 1;
+	}
+	
+	cout << dot * dot;
+
+	return 0;
+}
