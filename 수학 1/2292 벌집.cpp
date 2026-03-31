6a9e922c5722f54bf0abbe34f2ec664be7b1659f
Adonis <wintiger0908@gmail.com>
2026-03-31 20:06:53 +0900

    2292 다시 풀기

---
+++ 수학 1/2292 벌집.cpp
@@ -0,0 +1,43 @@
+#include<stdio.h>
+
+int main() {
+	int n;
+	scanf_s("%d", &n);
+
+	int ret = 1, max = 1, add = 6;
+
+	while (1) {
+		if (n <= max) break;
+
+		max += add;
+		add += 6;
+		++ret;
+	}
+
+	printf("%d\n", ret);
+	return 0;
+}
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	int n;
+
+	cin >> n;
+
+	int ret = 1, max = 1, add = 6;
+
+	while (1)
+	{
+		if (n <= max) break;
+		max += add;
+		add += 6;
+		++ret;
+	}
+
+	cout << ret;
+
+	return 0;
+}
