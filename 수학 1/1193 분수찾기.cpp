a50bf76d98b632ce4abd4a05d930c78b530aad8e
Adonis <wintiger0908@gmail.com>
2026-04-03 20:48:40 +0900

    1193 문제 풀이

--- 수학 1/1193 분수찾기.cpp
+++ 수학 1/1193 분수찾기.cpp
@@ -5,9 +5,53 @@ int main() {
 
 	scanf_s("%d", &X);
 
-	int count = 1, index = 1;
+	int idx = 1;
 
+	while (true)
+	{
+		if (idx * (idx - 1) / 2 < X && X <= idx * (idx + 1) / 2) break;
+		idx++;
+	}
+	
+	if (idx % 2 != 0)
+	{
+		int temp = idx * (idx + 1) / 2;
+		printf("%d", temp - X + 1);
+		printf("/");
+		printf("%d", idx - (temp - X));
+	}
+	else
+	{
+		int temp = idx * (idx + 1) / 2;
+		printf("%d", idx - (temp - X));
+		printf("/");
+		printf("%d", temp - X + 1);
+	}
 
+	return 0;
+}
 
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	int X, idx = 1;
+
+	cin >> X;
+
+	while (true)
+	{
+		if (idx * (idx - 1) / 2 < X && X <= idx * (idx + 1) / 2) break;
+		idx++;
+	}
+
+	int temp = idx * (idx + 1) / 2;
+
+	if (idx % 2 != 0)
+		cout << temp - X + 1 << "/" << idx - (temp - X);
+	else
+		cout << idx - (temp - X) << "/" << temp - X + 1;
+	
 	return 0;
 }
